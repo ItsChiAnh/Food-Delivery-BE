cd5cbe92321b05d2ef4c820813c8624fbcf6f796
petedaogit <namdao.0908@gmail.com>
2024-12-04 19:24:01 +0700

    updated editfood new

--- src/controllers/food.controllers.js
+++ src/controllers/food.controllers.js
@@ -59,19 +59,27 @@ const findFoodByID = async (req, res) => {
 
 const editFood = async (req, res) => {
   try {
-    const foodId = req.params.id || req.body.id;
+    const foodId = req.params.id || req.body.id; // Check for ID in both params and body (adjust based on your API design)
     const food = await foodModel.findById(foodId);
     if (!food) {
       return res.status(404).json({ message: "Food not found!" });
     }
 
+    // Update food properties (handle optional data)
+
     food.name = req.body.name || food.name;
     food.description = req.body.description || food.description;
     food.price = req.body.price || food.price;
     food.category = req.body.category || food.category;
-    console.log(food);
-    console.log(req.body);
 
+    // Update image if a new one is uploaded
+    if (req.file) {
+      food.image = `${req.file.filename}`;
+
+      // Implement logic to delete the old image file (if applicable)
+    }
+
+    // Save the updated food document
     await food.save();
     return res.status(200).json({ success: true, message: "Food updated!" });
   } catch (error) {
