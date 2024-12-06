7eb1b9a0b143aa0a6f73d1f11d67a2734a77766e
petedaogit <namdao.0908@gmail.com>
2024-12-06 17:50:16 +0700

    new

--- src/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("DB connection established!");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URI}/products`);
-};
-export default connectDB;
