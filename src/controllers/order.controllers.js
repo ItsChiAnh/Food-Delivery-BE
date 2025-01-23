65fc0ea226467762621c65f71d7ddc9c9a4f0847
petedaogit <namdao.0908@gmail.com>
2025-01-23 18:58:59 +0700

    link frontend ordercontroller

--- src/controllers/order.controllers.js
+++ src/controllers/order.controllers.js
@@ -6,7 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-delivery-fe-raqc.onrender.com";
 
   try {
     const newOrder = new orderModel({
