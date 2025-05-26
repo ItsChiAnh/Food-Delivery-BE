6b12a516f8d5e8a39cdb2009c455581f679e21a9
petedaogit <namdao.0908@gmail.com>
2025-05-26 14:43:59 +0700

    auth revert

--- src/middleware/auth.mdw.js
+++ src/middleware/auth.mdw.js
@@ -1,46 +1,31 @@
 import jwt from "jsonwebtoken";
-
-const extractToken = (authorizationHeader) => {
-  if (!authorizationHeader) {
-    return null;
-  }
-  const parts = authorizationHeader.split("");
-  if (parts.length === 2 && parts[0] === "Bearer") {
-    return parts[1];
-  }
-  return null;
-};
-
 const authorizationToken = async (req, res, next) => {
   try {
-    const token = extractToken(req.headers.authorization);
-    if (!token) {
-      res.status(401).json({
-        Message: "Invalid or missing Bearer token",
+    if (!req.headers.authorization) {
+      res.status(403).json({
+        Message: "Missing Bearer token",
       });
     }
 
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-      if (err) {
-        if (err instanceof jwt.TokenExpiredError) {
-          return res.status(401).json({ message: "Token expired" });
-        }
-        if (err instanceof jwt.JsonWebTokenError) {
-          return res.status(401).json({ message: "Invalid token" });
-        } else {
-          return res.status(401).json({ message: "Authentication failed" });
-        }
-      }
-    });
+    const token = req.headers.authorization.split("Bearer ")[1];
+    if (!token) {
+      res.status(403).json({
+        Message: "Invalid Bearer",
+      });
+    }
+    const user = jwt.verify(token, process.env.SECRET_KEY);
+    if (!user) {
+      res.status(403).json({
+        Message: "Invalid token",
+      });
+    }
+    req.user = user;
 
-    req.user = decoded;
-
-    console.log("~ authorizationToken ~ user: ", decoded);
+    console.log("~ authorizationToken ~ user: ", user);
     next();
   } catch (error) {
-    return res.status(500).json({
-      // Message: error.Message,
-      message: "Internal server error",
+    return res.status(401).json({
+      Message: error.Message,
     });
   }
 };
